7f61210dcf0444a8f6574cd9724e4657dbc1ee51
abaya7721 <62572548+abaya7721@users.noreply.github.com>
2025-02-18 08:58:32 -0700

    Created project files, connected database, created Product class

---
+++ inventorymanager/src/main/java/com/app/inventorymanager/model/Product.java
@@ -0,0 +1,70 @@
+package com.app.inventorymanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+    private String name;
+    private BigDecimal price;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
---
+++ inventorymanager/src/test/java/com/app/inventorymanager/InventoryManagementAppTests.java
@@ -0,0 +1,13 @@
+package com.app.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
